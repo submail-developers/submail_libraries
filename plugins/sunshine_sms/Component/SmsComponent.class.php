a67b1bdb7e475afd01f81f9c64bb0a8e42915fa5
xsqua <cyan8@live.com>
2020-05-28 17:17:30 +0800

    modify sunshine

--- plugins/sunshine_sms/Component/SmsComponent.class.php
+++ plugins/sunshine_sms/Component/SmsComponent.class.php
@@ -8,21 +8,6 @@ class SmsComponent {
 		$settings_product = sunshine_huayueModuleSite::$_SET['alidayu_product'];
 		$settings_product = $settings_product ? $settings_product : '快来租我';
 		$content = str_replace(['${product}','${code}'],["$settings_product","$captcha"],sunshine_huayueModuleSite::$_SET['alidayu_tpl_id']);
-
-        $statusStr = array(
-            "0" => "短信发送成功",
-            "-1" => "参数不全",
-            "-2" => "服务器空间不支持,请确认支持curl或者fsocket，联系您的空间商解决或者更换空间！",
-            "30" => "密码错误",
-            "40" => "账号不存在",
-            "41" => "余额不足",
-            "42" => "帐户已过期",
-            "43" => "IP地址限制",
-            "50" => "内容含有敏感词"
-		);
-
-
-
         $content="【".sunshine_huayueModuleSite::$_SET['alidayu_sign_name']."】".$content;
 		$phone = $mobile;//要发送短信的手机号码
 
